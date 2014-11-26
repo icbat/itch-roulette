6397e2d26196d868ef998fc93be9c9cae3304852
icbat <cobb.evan@gmail.com>
2014-11-26 14:22:45 -0600

    Refactoring

--- roulette/itch_interface.py
+++ roulette/itch_interface.py
@@ -1,9 +1,15 @@
-import urllib2 as url
+import urllib2
 import xml.etree.ElementTree as ET
 from game import Game
 
 def get_games():
-	page = url.urlopen('https://itch.io/games/price-500.xml')
+	return get_games_from_url("https://itch.io/games/price-500.xml")
+
+def get_games_from_url(url):
+	page = urllib2.urlopen(url)
+	return parse_xml(page)
+
+def parse_xml(page):
 	games_found = []
 	raw_xml = page.read()
 	root = ET.fromstring(raw_xml)
@@ -13,7 +19,7 @@ def get_games():
 		price = parse_one_value(item, "price")[1:]
 		if (currency == 'USD'):
 			games_found.append(Game(link, currency, price))
-	return games_found
+	return games_found	
 
 def parse_one_value(element, tag):
 	return element.findall(tag)[0].text
