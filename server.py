385647e45798253298efe63643612ca3e882e4dd
icbat <cobb.evan@gmail.com>
2014-11-26 15:38:58 -0600

    CLI args exist and are parsed

--- server.py
+++ server.py
@@ -2,6 +2,7 @@
 from settings import constants
 from settings import server_config
 from roulette import roulette
+import argparse
 
 from flask import Flask
 from flask import render_template
@@ -16,6 +17,17 @@ def hello():
 @app.route(api_endpoint + "games-under/<int:dollars>")
 def get_games(dollars):
 	return render_template("buy.html", games=roulette.play(dollars))
+
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--port', type=int, default="5000")
+parser.add_argument('--ip', default="")
+
+args = parser.parse_args()
+print args
+print args.port
+print args.ip
 
 if __name__ == "__main__":
     app.run(
