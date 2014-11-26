b641a26e09cfc4aba3b1a0b3f96636ad2e231a5f
icbat <cobb.evan@gmail.com>
2014-11-26 13:47:14 -0600

    Can parse a tiny bit.

--- roulette/itch_interface.py
+++ roulette/itch_interface.py
@@ -1,6 +1,15 @@
-import urllib2
+import urllib2 as url
+import xml.etree.ElementTree as ET
+import game
 
 def get_games():
-	page = urllib2.urlopen('https://itch.io/games/price-500.xml')
+	page = url.urlopen('https://itch.io/games/price-500.xml')
 	raw_xml = page.read()
+	root = ET.fromstring(raw_xml)
+	for item in root.findall('item'):
+		print item
+		print parse_one_value(item, "link")
 	return "games. I promise"
+
+def parse_one_value(element, tag):
+	return element.findall(tag)[0].text
