eb412747711348dfe8d69f88e688feb0aa80ce2b
icbat <cobb.evan@gmail.com>
2014-11-26 14:12:49 -0600

    create list of buy buttons

--- roulette/itch_interface.py
+++ roulette/itch_interface.py
@@ -10,7 +10,9 @@ def get_games():
 	for item in root.findall('item'):
 		link = parse_one_value(item, "link")
 		currency = parse_one_value(item, "currency")
-		price = parse_one_value(item, "price")
+		price = parse_one_value(item, "price")[1:]
+
+		# Had trouble w/ the GPB and Euro symbol being unicode and not parsing
 		if (currency == 'USD'):
 			games_found.append(Game(link, currency, price))
 	return games_found
