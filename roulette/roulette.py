feec7f4b89cdf82f7f0f0ef202e5922f4603bb64
icbat <cobb.evan@gmail.com>
2014-11-26 14:14:26 -0600

    Games randomize

--- roulette/roulette.py
+++ roulette/roulette.py
@@ -1,7 +1,9 @@
 import itch_interface
+import random
 
 def play(dollars):
 	games = itch_interface.get_games()
+	random.shuffle(games)
 	string = ""
 	for game in games:
 		string += str(game.price)
