4b6875a98623d7f0626f83b69facf3fc83038c3b
icbat <cobb.evan@gmail.com>
2014-11-26 14:16:01 -0600

    refactoring a bit

--- roulette/roulette.py
+++ roulette/roulette.py
@@ -4,6 +4,10 @@ import random
 def play(dollars):
 	games = itch_interface.get_games()
 	random.shuffle(games)
+	return convert_for_display(games)
+
+""" Takes a list of games and formats it for web display """
+def convert_for_display(games):
 	string = ""
 	for game in games:
 		string += str(game.price)
