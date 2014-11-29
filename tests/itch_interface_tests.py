710e08848eb4b7d9199d5bd1ed0381d8fafdab03
icbat <cobb.evan@gmail.com>
2014-11-28 21:03:50 -0600

    adding "negative" test

--- tests/itch_interface_tests.py
+++ tests/itch_interface_tests.py
@@ -2,8 +2,6 @@
 from roulette.game import Game
 import roulette.itch_interface as itch_interface
 
-
-
 def test_remove_duplicates_happy():
 	games = []
 	first_game = Game("icbat", "pressing", "some currency", "1234", "display_name", "image url")
@@ -19,3 +17,18 @@ def test_remove_duplicates_happy():
 	assert len(trimmed) == 1
 	assert trimmed[0].user == "icbat"
 
+def test_remove_duplicates_no_work():
+	games = []
+	first_game = Game("icbat", "pressing", "some currency", "1234", "display_name", "image url")
+	second_game = Game("someone else", "a better game", "a different currency", "4567", "disp", "url")
+	games.append(first_game)
+	games.append(second_game)
+	assert len(games) == 2
+
+	trimmed = itch_interface.remove_duplicates(games)
+
+	# assert no side-effects
+	assert len(games) == 2
+	assert len(trimmed) == 2
+	assert trimmed[0].user == "icbat"
+	assert trimmed[1].user == "someone else"
